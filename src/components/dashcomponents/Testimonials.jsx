e3cfa5b5edd82b9463ccc5c546f23d442d2ea964
Fortunechinenyem <fortunechinenyem@gmail.com>
2022-09-14 14:34:57 +0100

    update

--- src/components/dashcomponents/Testimonials.jsx
+++ src/components/dashcomponents/Testimonials.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import avatarImage from "./assets/lady.png";
 import { cardStyle } from "./ReusableStyles";
+import { Link } from "react-router-dom";
 
 export default function Testimonials() {
   const transactions = [
